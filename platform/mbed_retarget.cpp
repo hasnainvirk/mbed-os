609ba7a288c36c45b1ef595fa6cf99e267e3b713
Hasnain Virk <hasnain.virk@arm.com>
2017-04-10 14:54:26 +0300

    Adding missing std identifier to various methods
    Some methods were missing std identifier which had been causing compilation errors
    for ARM_BEETLE_SOC platform.

--- platform/mbed_retarget.cpp
+++ platform/mbed_retarget.cpp
@@ -244,7 +244,7 @@ extern "C" FILEHANDLE PREFIX(_open)(const char* name, int openmode) {
     /* FILENAME: ":0x12345678" describes a FileHandle* */
     if (name[0] == ':') {
         void *p;
-        sscanf(name, ":%p", &p);
+        std::sscanf(name, ":%p", &p);
         res = (FileHandle*)p;
 
     /* FILENAME: "/file_system/file_name" */
@@ -944,7 +944,7 @@ int __wrap_atexit(void (*func)()) {
 
 namespace mbed {
 
-static void mbed_set_unbuffered_stream(FILE *_file) {
+static void mbed_set_unbuffered_stream(std::FILE *_file) {
 #if defined (__ICCARM__)
     char buf[2];
     std::setvbuf(_file,buf,_IONBF,NULL);
@@ -967,7 +967,7 @@ std::FILE *fdopen(FileHandle *fh, const char *mode)
     return stream;
 }
 
-int mbed_getc(FILE *_file){
+int mbed_getc(std::FILE *_file){
 #if defined (__ICCARM__)
     /*This is only valid for unbuffered streams*/
     int res = std::fgetc(_file);
@@ -982,7 +982,7 @@ int mbed_getc(FILE *_file){
 #endif   
 }
 
-char* mbed_gets(char*s, int size, FILE *_file){
+char* mbed_gets(char*s, int size, std::FILE *_file){
 #if defined (__ICCARM__)
     /*This is only valid for unbuffered streams*/
     char *str = fgets(s,size,_file);
