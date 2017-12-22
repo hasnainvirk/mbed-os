b328a211a9fe9bf85a4b1d5fdc1f20e2e1e355a2
Kimmo Vaisanen <kimmo.vaisanen@arm.com>
2017-12-22 13:56:57 +0200

    Change the parameter Window of RxConfigParams_t into the LoRaMacRxSlot_t data type

--- features/lorawan/lorastack/phy/LoRaPHYAU915.cpp
+++ features/lorawan/lorastack/phy/LoRaPHYAU915.cpp
@@ -638,7 +638,7 @@ bool LoRaPHYAU915::rx_config(RxConfigParams_t* rxConfig, int8_t* datarate)
         return false;
     }
 
-    if (rxConfig->Window == 0) {
+    if (rxConfig->RxSlot == RX_SLOT_WIN_1) {
         // Apply window 1 frequency
         frequency = AU915_FIRST_RX1_CHANNEL
                 + (rxConfig->Channel % 8) * AU915_STEPWIDTH_RX1_CHANNEL;
