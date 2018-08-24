e86cec0e9ea851fdeab3aaf8e3c71f051d87fab8
Antti Kauppila <antti.kauppila@arm.com>
2018-08-24 15:12:48 +0300

    Rejoin logic added

--- features/lorawan/LoRaWANStack.h
+++ features/lorawan/LoRaWANStack.h
@@ -544,7 +544,6 @@ private:
     uint8_t _rx_payload[LORAMAC_PHY_MAXPAYLOAD];
     events::EventQueue *_queue;
     lorawan_time_t _tx_timestamp;
-
     uint32_t _rejoin_type1_send_period;
     uint32_t _rejoin_type1_stamp;
     timer_event_t _rejoin_type0_timer;
